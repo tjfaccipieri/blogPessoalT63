4f9a1fac9e296849daff238bd2a1732987f1b3ac
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2023-05-15 07:51:40 -0300

    :lipstick: Adicionado navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './componentes/estaticos/navbar/Navbar'
 import Home from './paginas/home/Home'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <Home />
     </>
   )
